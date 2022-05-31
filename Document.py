1177f92697e457de7dc15cd6f00d4b988d9da2a2
Ben-Ferry <bbferry@uw.edu>
2022-05-30 17:12:28 -0700

    added document and rename
    added document.py and editied names

---
+++ Document.py
@@ -0,0 +1,10 @@
+"""
+Read in csv data and creating data frames.
+"""
+import pandas as pd
+
+chloro2014 = pd.read_csv("/CSE-163-Final/Data_csv/2014_Chloro.csv")
+chloro2015 = pd.read_csv("/CSE-163-Final/Data_csv/2015_Chloro.csv")
+chloro2016 = pd.read_csv("/CSE-163-Final/Data_csv/2016_Chloro.csv")
+chloro2017 = pd.read_csv("/CSE-163-Final/Data_csv/2017_Chloro.csv")
+chloro2018 = pd.read_csv("/CSE-163-Final/Data_csv/2018_Chloro.csv")
--- main.py
+++ main.py
@@ -1,3 +1,17 @@
 """
-This is the file for working
+This is the main file for processing data and visualizing it.
 """
+import pandas as pd
+
+
+
+
+
+def main():
+    """
+    This is the main function.
+    """
+
+
+if __name__ == '__main__':
+    main()

1a8a87ef01fc4fc9f3316c6a6546c72e5a601984
Ben-Ferry <bbferry@uw.edu>
2022-06-01 18:02:42 -0700

    added 2008-2013 data
    Implemented geopandas map of chloropricrin

--- main.py
+++ main.py
@@ -4,10 +4,12 @@ This is the main file for processing data and visualizing it.
 import pandas as pd
 import geopandas as gpd
 import matplotlib.pyplot as plt
+import altair as alt
 
 
 def load_in_data(shp_file_towns, chloro2014, chloro2015, chloro2016,
-                 chloro2017, chloro2018):
+                 chloro2017, chloro2018, data2013, data2010, data2009,
+                 data2008, data2012, data2011):
     sb_shape_towns = gpd.read_file(shp_file_towns)
     sb_shape_towns = sb_shape_towns[["CO_MTRS", "geometry"]]
     chloro2014 = pd.read_csv(chloro2014)
@@ -15,7 +17,14 @@ def load_in_data(shp_file_towns, chloro2014, chloro2015, chloro2016,
     chloro2016 = pd.read_csv(chloro2016)
     chloro2017 = pd.read_csv(chloro2017)
     chloro2018 = pd.read_csv(chloro2018)
-    chloro_csv = [chloro2014, chloro2015, chloro2016, chloro2017, chloro2018]
+    data2013 = pd.read_csv(data2013)
+    data2010 = pd.read_csv(data2010)
+    data2009 = pd.read_csv(data2009)
+    data2008 = pd.read_csv(data2008)
+    data2012 = pd.read_csv(data2012)
+    data2011 = pd.read_csv(data2011)
+    chloro_csv = [chloro2014, chloro2015, chloro2016, chloro2017, chloro2018,
+                  data2013, data2010, data2009, data2008, data2012, data2011]
     chloro_csv = pd.concat(chloro_csv)
     chemical_chloro = chloro_csv["CHEMICAL NAME"] == "CHLOROPICRIN"
     county = chloro_csv["COUNTY NAME"] == "SANTA BARBARA"
@@ -23,13 +32,16 @@ def load_in_data(shp_file_towns, chloro2014, chloro2015, chloro2016,
                                 ["YEAR", "COUNTY NAME",
                                  "COMTRS", "CHEMICAL NAME",
                                  "POUNDS CHEMICAL APPLIED"]]
-    chloro_data = chloro_csv.merge(sb_shape_towns, left_on="COMTRS",
-                                   right_on="CO_MTRS", how="left")
+    chloro_data = sb_shape_towns.merge(chloro_csv, left_on="CO_MTRS",
+                                       right_on="COMTRS", how="left")
     return chloro_data
 
 
 def plot_santa_barbara_sections(chloro_data):
-    print(chloro_data)
+    fig, ax = plt.subplots(1)
+    chloro_data.plot(ax=ax, color="#EEEEEE")
+    chloro_data.plot(ax=ax, column="POUNDS CHEMICAL APPLIED", legend=True)
+    plt.savefig("map.png")
 
 
 def main():
@@ -47,7 +59,19 @@ def main():
                             "~/Desktop/GitHub/CSE-163-Final/"
                             "Data_csv/2017_Chloro.csv",
                             "~/Desktop/GitHub/CSE-163-Final/"
-                            "Data_csv/2018_Chloro.csv")
+                            "Data_csv/2018_Chloro.csv",
+                            "~/Desktop/GitHub/CSE-163-Final/"
+                            "Data_csv/2013_data.csv",
+                            "~/Desktop/GitHub/CSE-163-Final/"
+                            "Data_csv/2010_data.csv",
+                            "~/Desktop/GitHub/CSE-163-Final/"
+                            "Data_csv/2009_data.csv",
+                            "~/Desktop/GitHub/CSE-163-Final/"
+                            "Data_csv/2008_data.csv",
+                            "~/Desktop/GitHub/CSE-163-Final/"
+                            "Data_csv/2012_data.csv",
+                            "~/Desktop/GitHub/CSE-163-Final/"
+                            "Data_csv/2011_data.csv")
     plot_santa_barbara_sections(chloro_data)
 
 
